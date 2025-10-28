c2c90b129c05641279532c3b0d3faac41c3d02bb
jaabless <jefferyasamoahbless@gmail.com>
2025-10-28 22:36:06 +0000

    feat(tests): add BusinessRegistrationTests for business registration page

--- src/test/java/com/carrentalservice/ui_tests/tests/BusinessRegistrationTests.java
+++ src/test/java/com/carrentalservice/ui_tests/tests/BusinessRegistrationTests.java
@@ -1,6 +1,30 @@
 package com.carrentalservice.ui_tests.tests;
 
-public class BusinessRegistrationTests {
+import com.carrentalservice.pages.BusinessRegistrationPage;
+import com.carrentalservice.pages.DecisionPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class BusinessRegistrationTests extends BaseTest{
 
+    @BeforeEach
+    public void setup() {
+       DecisionPage decisionPage = loginPage.clickRegister();
+       decisionPage.clickRegisterBusiness();
+    }
+
+    @Test
+    public void testValidBusinessRegistration() {
+        BusinessRegistrationPage businessRegistrationPage = new BusinessRegistrationPage(driver);
+        businessRegistrationPage.completeBusinessRegistration("Full name", "email@email.com", "+233245323651", "Password1!", "Password1!", "Business Name", "123 Business St");
+        System.out.println("Running testValidBusinessRegistration");
+        // Implement test logic for valid business registration
+    }
+
+    @Test
+    public void testInvalidBusinessRegistration() {
+        BusinessRegistrationPage businessRegistrationPage = new BusinessRegistrationPage(driver);
+        System.out.println("Running testInvalidBusinessRegistration");
+        // Implement test logic for invalid business registration
+    }
 }
