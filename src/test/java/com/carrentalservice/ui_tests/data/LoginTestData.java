2c481b4e865251f9c522a94e813b58e5d960eac1
jaabless <jefferyasamoahbless@gmail.com>
2025-11-10 10:39:37 +0000

    feat(test): update test data

--- src/test/java/com/carrentalservice/ui_tests/data/LoginTestData.java
+++ src/test/java/com/carrentalservice/ui_tests/data/LoginTestData.java
@@ -5,9 +5,11 @@ import java.util.stream.Stream;
 public class LoginTestData {
     public static Stream<Object[]> validLoginTestData() {
         return Stream.of(
-                new Object[]{"test.user@example.com", "ValidPass123!", true, "Login successful"},
-                new Object[]{"newbusinessemail@gmail.com", "newBusiness1@", true, "Login successful"},
-                new Object[]{"newcustomeremail@gmail.com", "newCustomer1@", true, "Login successful"}
+                new Object[]{"jefferytechie@gmail.com", "Password@123", true, "Login successful"},
+                new Object[]{"jefferytechie@gmail.com", "Password@123", true, "Login successful"},
+                new Object[]{"newbusinessemail@gmail.com", "SecurePass123!", true, "Login successful"},
+                new Object[]{"newcustomeremail@gmail.com", "SecurePass123!", true, "Login successful"},
+                new Object[]{"test@example.com", "SecurePass123!", true, "Login successful"}
         );
     }
 
--- src/test/java/com/carrentalservice/ui_tests/data/RegisterBusinessTestData.java
+++ src/test/java/com/carrentalservice/ui_tests/data/RegisterBusinessTestData.java
@@ -16,22 +16,22 @@ public class RegisterBusinessTestData {
 
     public static Stream<Arguments> invalidAccountData() {
         return Stream.of(
-                Arguments.of("", "test@example.com", "+233245323651", "SecurePass123!!", "SecurePass123!", "Full name is required"),
+                Arguments.of("", "test@example.com", "+233245323651", "SecurePass123!", "SecurePass123!", "Full name is required"),
                 Arguments.of("John Doe", "", "+233245323651", "SecurePass123!", "SecurePass123!", "Email is required"),
                 Arguments.of("John Doe", "invalid", "+233245323651", "SecurePass123!", "SecurePass123!", "Email invalid"),
                 Arguments.of("John Doe", "test@example.com", "", "SecurePass123!", "SecurePass123!!", "Phone number is required"),
-                Arguments.of("John Doe", "test@example.com", "12345678", "SecurePass123!", "SecurePass123!", "Enter a valid phone number"),
+                Arguments.of("John Doe", "test@example.com", "12345678", "SecurePass123!", "SecurePass123!", "Invalid phone number"),
                 Arguments.of("John Doe", "test@example.com", "+233245323651", "pass", "SecurePass123!", "Minimum of 8 characters, uppercase, number and a symbol"),
-                Arguments.of("John Doe", "test@example.com", "+233245323651", "SecurePass123!", "", "Confirm password is required"),
+                Arguments.of("John Doe", "test@example.com", "+233245323651", "SecurePass123!", "", "Your passwords do not match"),
                 Arguments.of("John Doe", "test@example.com", "+233245323651", "SecurePass123!", "WrongPass", "Your passwords do not match")
         );
     }
 
     public static Stream<Arguments> validAccountData() {
         return Stream.of(
-                Arguments.of("John Doe", "test@example.com", "+233245323651", "SecurePass123!!", "SecurePass123!"),
-                Arguments.of("Kwame John", "test@example.com", "+233245323123", "SecurePass123!!", "SecurePass123!"),
-                Arguments.of("John Kwame", "test@example.com", "+233245323451", "SecurePass123!!", "SecurePass123!")
+                Arguments.of("John Doe", "newbusinessemail@gmail.com", "+233245323651", "SecurePass123!", "SecurePass123!"),
+                Arguments.of("Kwame John", "newcustomeremail@gmail.com", "+233245323123", "SecurePass123!", "SecurePass123!"),
+                Arguments.of("John Kwame", "test@example.com", "+233245323451", "SecurePass123!", "SecurePass123!")
         );
     }
 
@@ -44,18 +44,38 @@ public class RegisterBusinessTestData {
 
     public static Stream<Arguments> invalidCompanyData() {
         return Stream.of(
-                Arguments.of("Acme Car Rentals", "src/test/resources/files/doc.pdf", "Ghana", "Los Angeles", "src/test/resources/files/doc.pdf"),
-                Arguments.of("Real Estate", "src/test/resources/files/logo.png", "Ghana", "Miami", "src/test/resources/files/logo.png"),
-                Arguments.of("", "src/test/resources/files/logo.png", "Ghana", "Miami", "src/test/resources/files/doc.pdf"),
-                Arguments.of("Real Estate", "src/test/resources/files/logo.png", "Ghana", "", "src/test/resources/files/logo.png")
-                );
+                Arguments.of(
+                        "John Doe", "john.doe@acme.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "", "src/test/resources/files/logo.png", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf", "Company Name is required"
+                ),
+                Arguments.of(
+                        "John Doe", "john.doe@acme.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "Company Name", "src/test/resources/files/logo.png", "Ghana", "", "src/test/resources/files/doc.pdf", "City / Location is required"
+                ),
+                Arguments.of(
+                        "John Doe", "john.doe@acme.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "My Company", "src/test/resources/files/doc.pdf", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf", "Invalid file type. Only .jpg, .png, .jpeg files are allowed."
+                )
+        );
     }
 
     public static Stream<Arguments> validRegistrationData() {
         return Stream.of(
                 Arguments.of(
-                        "John Doe", "john.doe@acme.com", "+233242531625", "SecurePass123!", "SecurePass123!",
-                        "Acme Car Rentals", "src/test/resources/files/logo.png", "Los Angeles", "src/test/resources/files/doc.pdf"
+                        "John Doe", "newbusinessemail@gmail.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "Acme Car Rentals", "src/test/resources/files/logo.png", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf"
+                ),
+                Arguments.of(
+                        "John Doe", "newcustomeremail@gmail.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "Acme Car Rentals", "src/test/resources/files/logo.png", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf"
+                ),
+                Arguments.of(
+                        "John Doe", "test@example.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "Acme Car Rentals", "src/test/resources/files/logo.png", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf"
+                ),
+                Arguments.of(
+                        "John Doe", "jefferytechie@gmail.com", "+233242531625", "SecurePass123!", "SecurePass123!",
+                        "Acme Car Rentals", "src/test/resources/files/logo.png", "Ghana", "Kumasi", "src/test/resources/files/doc.pdf"
                 )
         );
     }
