948086a6443284bf5fecf84926eda1d53017eee8
jaabless <jefferyasamoahbless@gmail.com>
2025-11-10 14:26:38 +0000

    feat(pages): add phomepage

---
+++ src/main/java/com/carrentalservice/pages/Homepage.java
@@ -0,0 +1,22 @@
+package com.carrentalservice.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Homepage extends BasePage {
+
+    @FindBy(xpath = "//span[normalize-space()='Login']")
+    private WebElement loginButton;
+
+    public Homepage(WebDriver driver) {
+        super(driver);
+    }
+
+    public LoginPage clickLogin() {
+        logger.info("Clicking login button on homepage");
+        loginButton.click();
+        return new LoginPage(driver);
+    }
+
+}
