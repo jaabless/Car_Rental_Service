24651391949bd98b6d7f95147ffea5ef96f2c6ce
jaabless <jefferyasamoahbless@gmail.com>
2025-11-10 10:54:04 +0000

    feat(config): update URL

--- src/main/java/com/carrentalservice/config/Config.java
+++ src/main/java/com/carrentalservice/config/Config.java
@@ -2,7 +2,7 @@ package com.carrentalservice.config;
 
 
 public class Config {
-    public static final String BASE_URL = "https://d2pnvbshv9sl2w.cloudfront.net/login";
+    public static final String BASE_URL = "https://d2pnvbshv9sl2w.cloudfront.net/auth/login";
     // Timeout seconds
     public static final long DEFAULT_TIMEOUT = 10;
 }
